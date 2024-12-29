7eab01c80a0e6acd69293ab2adf97b55d22a7135
AkiTheMemeGod <k.akashkumar@gmail.com>
2024-12-29 12:01:31 +0530

    Add authentication functionality with email and username signup/signin

--- Database.py
+++ Database.py
@@ -1,4 +1,60 @@
 import sqlite3 as sq
 
-class ProtoBaseAuthentication():
-    pass
+
+class Helpers:
+    def __init__(self, con=None):
+        if con:
+            self.con = con
+        else:
+            self.con = sq.connect("Authentication.db", check_same_thread=False)
+
+    def duplicate_email_check(self, email):
+        cur = self.con.cursor()
+        cur.execute("SELECT email FROM authwithemail")
+        emails = cur.fetchall()
+        emails = [i[0] for i in emails]
+        return email in emails
+
+    def duplicate_username_check(self, username):
+        cur = self.con.cursor()
+        cur.execute("SELECT email FROM authwithemail")
+        usernames = cur.fetchall()
+        usernames = [i[0] for i in usernames]
+        return username in usernames
+
+
+class ProtoBaseAuthentication(Helpers):
+
+    def signup_with_email(self, username, password, email):
+        cur = self.con.cursor()
+        if not self.duplicate_email_check(email):
+            cur.execute("INSERT INTO authwithemail VALUES (?,?,?)", (username, password, email))
+            self.con.commit()
+            self.con.close()
+            return 200
+        else:
+            return 400
+
+    def signup_with_username(self, username, password):
+        cur = self.con.cursor()
+        if not self.duplicate_username_check(username):
+            cur.execute("INSERT INTO authwithoutemail VALUES (?,?)", (username, password))
+            self.con.commit()
+            self.con.close()
+            return 200
+        else:
+            return 400
+
+    def signin_with_email(self, username, password, email):
+        cur = self.con.cursor()
+        cur.execute(f"SELECT password, email FROM authwithemail WHERE username=?", (username, ))
+        details = cur.fetchone()
+        return (password, email, ) == details
+
+    def signin_with_username(self, username, password):
+        cur = self.con.cursor()
+        cur.execute(f"SELECT password FROM authwithoutemail WHERE username=?", (username,))
+        details = cur.fetchone()
+        return (password,) == details
+
+
--- app.py
+++ app.py
@@ -1,11 +1,91 @@
 from flask import *
-
+import sqlite3 as sq
+from Database import ProtoBaseAuthentication
 app = Flask(__name__)
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sq.connect("Authentication.db")
+    return g.db
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
 def homepage():
     return render_template("index.html")
+
+
+@app.route("/auth_api/email-signup/")
+def signup_email():
+    usr = request.args.get('usr')
+    pwd = request.args.get('pwd')
+    email = request.args.get('email')
+    con = get_db()
+    db = ProtoBaseAuthentication(con)
+    if usr and pwd and email:
+        s = db.signup_with_email(usr, pwd, email)
+
+        if s == 200:
+            return jsonify({"message": "User Successfully SignedUp"}), 200
+        else:
+            return jsonify({"message": "Username Already Exists"}), 400
+    else:
+        return jsonify({"message": "Invalid input parameters"}), 400
+
+
+@app.route("/auth_api/user-signup/")
+def signup_username():
+    usr = request.args.get('usr')
+    pwd = request.args.get('pwd')
+    con = get_db()
+    db = ProtoBaseAuthentication(con)
+    if usr and pwd:
+        s = db.signup_with_username(usr, pwd)
+        if s == 200:
+            return jsonify({"message": "User Successfully SignedUp"}), 200
+        else:
+            return jsonify({"message": "Username Already Exists"}), 400
+
+    else:
+        return jsonify({"message": "Invalid input parameters"}), 400
+
+
+@app.route("/auth_api/email-signin/")
+def signin_email():
+    usr = request.args.get('usr')
+    pwd = request.args.get('pwd')
+    email = request.args.get('email')
+    con = get_db()
+    db = ProtoBaseAuthentication(con)
+    if usr and pwd and email:
+        if db.signin_with_email(usr, pwd, email):
+            return jsonify({"message": "User Successfully Logged in"}), 200
+        else:
+            return jsonify({"message": "Incorrect Credentials"}), 400
+    else:
+        return jsonify({"message": "Invalid input parameters"}), 400
+
+
+@app.route("/auth_api/user-signin/")
+def signin_username():
+    usr = request.args.get('usr')
+    pwd = request.args.get('pwd')
+    con = get_db()
+    db = ProtoBaseAuthentication(con)
+    if usr and pwd:
+        if db.signin_with_username(usr, pwd):
+            return jsonify({"message": "User Successfully Logged in"}), 200
+        else:
+            return jsonify({"message": "Incorrect Credentials"}), 400
+    else:
+        return jsonify({"message": "Invalid input parameters"}), 400
 
 
 if __name__ == '__main__':
