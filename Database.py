b2349d12c0b5243122b36f4b7b0e1c4f3b8b2fa4
AkiTheMemeGod <k.akashkumar@gmail.com>
2024-12-31 09:19:10 +0530

    Add signin and signup pages with corresponding styles and functionality

--- Database.py
+++ Database.py
@@ -1,4 +1,6 @@
+import sqlite3
 import sqlite3 as sq
+import secrets as st
 
 
 class Helpers:
@@ -15,55 +17,103 @@ class Helpers:
         emails = [i[0] for i in emails]
         return email in emails
 
-    def duplicate_username_check(self, username):
+    def duplicate_username_check(self, username, table):
         cur = self.con.cursor()
-        cur.execute("SELECT email FROM authwithemail")
+        cur.execute(f"SELECT username FROM {table}")
         usernames = cur.fetchall()
         usernames = [i[0] for i in usernames]
         return username in usernames
 
 
-class ProtoBaseAuthentication(Helpers):
+class ApiToken(Helpers):
 
-    def signup_with_email(self, username, password, email):
+    @staticmethod
+    def generate_api_token():
+        token = st.token_urlsafe(64)
+        return token
+
+    def assign_api_token(self, username, password):
         cur = self.con.cursor()
-        if not self.duplicate_email_check(email):
-            cur.execute("INSERT INTO authwithemail VALUES (?,?,?)", (username, password, email))
-            self.con.commit()
-            self.con.close()
-            return 200
+        if not self.duplicate_username_check(username, "dev_api_tokens"):
+            token = self.generate_api_token()
+            try:
+                cur.execute("INSERT INTO dev_api_tokens VALUES (?, ?, ?, ?)", (username, password, token, 0))
+                self.con.commit()
+                return True, token
+            except sqlite3.IntegrityError:
+                return False, "Database error while assigning token."
         else:
-            return 400
+            cur.execute('SELECT token FROM dev_api_tokens WHERE username=?', (username,))
+            token = cur.fetchone()[0]
+            return True, token
 
-    def signup_with_username(self, username, password):
+    def validate_token(self, token):
         cur = self.con.cursor()
-        if not self.duplicate_username_check(username):
-            cur.execute("INSERT INTO authwithoutemail VALUES (?,?)", (username, password))
-            self.con.commit()
-            self.con.close()
-            return 200
-        else:
-            return 400
+        cur.execute("SELECT token FROM dev_api_tokens")
+        tokens = cur.fetchall()
+        tokens = [i[0] for i in tokens]
+        return token in tokens
 
-    def signin_with_email(self, username, password, email):
-        cur = self.con.cursor()
-        cur.execute(f"SELECT password, email FROM authwithemail WHERE username=?", (username, ))
-        details = cur.fetchone()
-        if not details:
-            return True, 400
-        elif (password, email, ) == details:
-            return True, 200
-        else:
-            return False, 400
 
-    def signin_with_username(self, username, password):
-        cur = self.con.cursor()
-        cur.execute(f"SELECT password FROM authwithoutemail WHERE username=?", (username,))
-        details = cur.fetchone()
-        if not details:
-            return True, 400
-        elif (password, ) == details:
-            return True, 200
-        else:
-            return False, 400
+class ProtoBaseAuthentication(ApiToken):
 
+    def signup_with_email(self, username, password, email, token):
+        if self.validate_token(token):
+
+            cur = self.con.cursor()
+            if not self.duplicate_email_check(email):
+                cur.execute("INSERT INTO authwithemail VALUES (?,?,?)", (username, password, email))
+                self.con.commit()
+                self.con.close()
+                return 200
+            else:
+                return 400
+        else:
+            return 500
+
+    def signup_with_username(self, username, password, token):
+        if self.validate_token(token):
+            cur = self.con.cursor()
+            if not self.duplicate_username_check(username, "authwithoutemail"):
+                cur.execute("INSERT INTO authwithoutemail VALUES (?,?)", (username, password))
+                self.con.commit()
+                self.con.close()
+                return 200
+            else:
+                return 400
+        else:
+            return 500
+
+    def signin_with_email(self, username, password, email, token):
+        if self.validate_token(token):
+
+            cur = self.con.cursor()
+            cur.execute(f"SELECT password, email FROM authwithemail WHERE username=?", (username, ))
+            details = cur.fetchone()
+            if not details:
+                return True, 400
+            elif (password, email, ) == details:
+                return True, 200
+            else:
+                return False, 400
+        else:
+            return True, 500
+
+    def signin_with_username(self, username, password, token):
+        if self.validate_token(token):
+
+            cur = self.con.cursor()
+            cur.execute(f"SELECT password FROM authwithoutemail WHERE username=?", (username,))
+            details = cur.fetchone()
+            if not details:
+                return True, 400
+            elif (password, ) == details:
+                return True, 200
+            else:
+                return False, 400
+        else:
+            return True, 500
+
+
+d = ApiToken()
+d.validate_token("adasdasd")
--- flask_app.py
+++ flask_app.py
@@ -21,9 +21,46 @@ def close_db(exception):
 def homepage():
     return render_template("index.html")
 
+
 @app.route('/get-started')
 def get_started():
     return render_template('get-started.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    elif request.method == 'POST':
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+        con = get_db()
+        db = ProtoBaseAuthentication(con)
+        status, token = db.assign_api_token(username, password)
+        if status:
+            return jsonify(success=True, api_token=token)
+        else:
+            return jsonify(success=False, message="Signup failed. User may already exist.")
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'GET':
+        return render_template('signin.html')
+    elif request.method == 'POST':
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+        con = get_db()
+        db = ProtoBaseAuthentication(con)
+        cur = con.cursor()
+        cur.execute("SELECT token FROM dev_api_tokens WHERE username=? AND password=?", (username, password))
+        result = cur.fetchone()
+        if result:
+            return jsonify(success=True, api_token=result[0])
+        else:
+            return jsonify(success=False, message="Signin failed. Incorrect credentials.")
 
 
 @app.route("/auth_api/email-signup/")
@@ -31,13 +68,16 @@ def signup_email():
     usr = request.args.get('usr')
     pwd = request.args.get('pwd')
     email = request.args.get('email')
+    token = request.args.get('token')
     con = get_db()
     db = ProtoBaseAuthentication(con)
     if usr and pwd and email:
-        s = db.signup_with_email(usr, pwd, email)
+        s = db.signup_with_email(usr, pwd, email, token)
 
         if s == 200:
             return jsonify({"message": "User Successfully SignedUp", "status-code": 400})
+        elif s == 500:
+            return jsonify({"message": "INVALID API TOKEN", "status-code": 500})
         else:
             return jsonify({"message": "Username Already Exists", "status-code": 400})
     else:
@@ -48,12 +88,16 @@ def signup_email():
 def signup_username():
     usr = request.args.get('usr')
     pwd = request.args.get('pwd')
+    token = request.args.get('token')
+
     con = get_db()
     db = ProtoBaseAuthentication(con)
     if usr and pwd:
-        s = db.signup_with_username(usr, pwd)
+        s = db.signup_with_username(usr, pwd, token)
         if s == 200:
             return jsonify({"message": "User Successfully SignedUp", "status-code": 200})
+        elif s == 500:
+            return jsonify({"message": "INVALID API TOKEN", "status-code": 500})
         else:
             return jsonify({"message": "Username Already Exists", "status-code": 400})
 
@@ -66,15 +110,20 @@ def signin_email():
     usr = request.args.get('usr')
     pwd = request.args.get('pwd')
     email = request.args.get('email')
+    token = request.args.get('token')
+
     con = get_db()
     db = ProtoBaseAuthentication(con)
     if usr and pwd and email:
-        status, code = db.signin_with_email(usr, pwd, email)
+        status, code = db.signin_with_email(usr, pwd, email, token)
         if status:
             if code == 200:
                 return jsonify({"message": "User Successfully Logged in", "status-code": 200})
             elif code == 400:
                 return jsonify({"message": "User Not Found", "status-code": 400})
+            elif code == 500:
+                return jsonify({"message": "INVALID API TOKEN", "status-code": 500})
+
         else:
             return jsonify({"message": "Incorrect Credentials", "status-code": 400})
     else:
@@ -85,15 +134,20 @@ def signin_email():
 def signin_username():
     usr = request.args.get('usr')
     pwd = request.args.get('pwd')
+    token = request.args.get('token')
+
     con = get_db()
     db = ProtoBaseAuthentication(con)
     if usr and pwd:
-        status, code = db.signin_with_username(usr, pwd)
+        status, code = db.signin_with_username(usr, pwd, token)
         if status:
             if code == 200:
                 return jsonify({"message": "User Successfully Logged in", "status-code": 200})
             elif code == 400:
                 return jsonify({"message": "User Not Found", "status-code": 400})
+            elif code == 500:
+                return jsonify({"message": "INVALID API TOKEN", "status-code": 500})
+
         else:
             return jsonify({"message": "Incorrect Credentials", "status-code": 400})
     else:
