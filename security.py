66cba79abdcffcfdc76a59d21473e2a25d9acc0c
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-01-03 10:39:15 +0530

    Implement OTP generation and email sending for user signup verification

--- security.py
+++ security.py
@@ -1,4 +1,9 @@
 import math
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import date
+import random as rd
 
 
 class ProtobaseSecurity:
@@ -33,3 +38,45 @@ class ProtobaseSecurity:
             encrypted_values.append(hex(enc_value))
 
         return "".join(encrypted_values)
+
+
+class Protobase2FA:
+    @staticmethod
+    def generate_otp():
+        otp = rd.randint(100000, 999999)
+        return otp
+
+    def send_mail(self, email):
+        otp = self.generate_otp()
+        subject = "Your Protobase Signup OTP Code"
+        body = (
+            f"Dear User,\n\n"
+            f"Thank you for signing up with Protobase. To complete your registration, please use the following One-Time Password (OTP):\n\n"
+            f"    {otp}\n\n"
+            f"This OTP is valid for the next 10 minutes. Please do not share this code with anyone.\n\n"
+            f"If you did not request this code, please ignore this email or contact our support team immediately.\n\n"
+            f"Best regards,\n"
+            f"The Protobase Team"
+        )
+
+        sender_email = "akis.pwdchecker@gmail.com"
+        sender_password = "tjjqhaifdobuluhg"
+
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = email
+        message["Subject"] = subject
+        message.attach(MIMEText(body, "plain"))
+
+        try:
+            # Connecting to the server
+            with smtplib.SMTP("smtp.gmail.com", 587) as server:
+                server.starttls()
+                server.login(sender_email, sender_password)
+                server.send_message(message)
+            # print("Email sent successfully.")
+            return True, otp
+        except Exception as e:
+            # print(f"Failed to send email: {e}")
+            return False, None
+
