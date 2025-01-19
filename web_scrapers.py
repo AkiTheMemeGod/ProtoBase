ad4b209b3e25754c9e811486c81974b84a02f198
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-01-19 21:58:32 +0530

    updated web_scrapers.py for error logging

--- web_scrapers.py
+++ web_scrapers.py
@@ -1,15 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-
+from requests.exceptions import ProxyError
 
 def pub_dev_downloads():
     url = "https://pub.dev/packages/proto_base_client/score"
-
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    downloads_element = soup.find("div", class_="packages-score packages-score-downloads")
-    downloads_value = downloads_element.find("span", class_="packages-score-value-number").text
-
-    return downloads_value
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
+        downloads_element = soup.find("div", class_="packages-score packages-score-downloads")
+        downloads_value = downloads_element.find("span", class_="packages-score-value-number").text
+        return downloads_value
+    except ProxyError as e:
+        # Log the error or handle it as needed
+        print(f"ProxyError: {e}")
+        return "N/A"  # Return a default value or handle accordingly
