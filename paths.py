69b1a6924886c076e24d40396f42d1c35e4f1718
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-02-06 15:31:48 +0530

    Changed Path to Adapt to any environment

--- paths.py
+++ paths.py
@@ -1,4 +1,7 @@
+import os
+
+x = os.getcwd()
 windows = "Authentication.db"
-linux = "/home/ProtoBase/ProtoBase/Authentication.db"
-static_lin = "/home/ProtoBase/ProtoBase/static/"
+linux = f"{x}/Authentication.db"
+static_lin = f"{x}/static/"
 static_win = "static/"
